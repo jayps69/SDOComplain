7b2196fa57d7f019c6cfef6f104f084f377e3085
Jayps69 <131273253+jayps69@users.noreply.github.com>
2023-09-27 02:46:12 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>School Division Office Complaints Management System</title>
+</head>
+<body>
+    <section id="banner"></section>
+    
+    <div class="container">
+    
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="com"><h1><b>WELCOME TO SCHOOLS DIVISION OFFICE COMPLAINT MANAGEMENT SYSTEM</b></h1>
+                <p><h2>The Easiest Way To Complain & Concerns </h2><br> <b><i>Tired off getting ripped off? Fight back and file a customer complaint now.Sign Up Today And You Can Leave Complaints on various topics/company.</b> </i></p></div>
+            </div>
+            <br>
+
+            <div class="button-container">
+                <!--<button class="btn" id="userLoginButton">USER LOGIN</button>-->
+                <button class="btn" id="adminLoginButton">ADMIN LOGIN</button>
+            </div>
+        </div>
+    <style>.com{text-align: center;} </style>
+    <br><br><br><br><br>
+    <!-- Footer -->
+    <footer>
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Copyright &copy; 2023 CMS for Shool Divison Office</p>
+            </div>
+        </div>
+    <!-- /.row -->
+    </footer>
+    <style> .col-lg-12{text-align: center; border: 1px solid black;}</style>
+
+    </div>
+    <!--
+    <script>
+        document.getElementById('userLoginButton').addEventListener('click', function() {
+            // Redirect to the user login page
+            window.location.href = 'user/userlogin.php';
+        });
+    </script> IF YOU WANT TO USED user page  CLEAR COMMENT TOGGLE-->
+    
+    <script>
+        document.getElementById('adminLoginButton').addEventListener('click', function() {
+            // Redirect to the user login page
+            window.location.href = 'admin/adminlogin.php';
+        });
+    </script>
+    
+</body>
+</html>
